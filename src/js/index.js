61e625529978a71ce249069c66c5f382e3c3460d
Danylo Kyrmach <tomahawk535@gmail.com>
2021-04-13 11:12:30 +0300

    add animation of benefits

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    var show = true;
+    var countbox = ".benefits-inner";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 700 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.benefits-number').css('opacity', '1');
+            $('.benefits-number').spincrement({
+                thousandSeparator: "",
+                duration: 1500,
+                fade: true
+            });
+
+            show = false;
+        }
+    });
+
+});
